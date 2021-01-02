59b686409ac2618725c7d47d7427b25b28d7fe5f
DungVu <cnttlc@gmail.com>
2021-01-02 09:31:51 +0700

    Update CRD.sh
    Update user and password

--- CRD.sh
+++ CRD.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing CRD-server... \nThis might take a while... " >&2
 {
-sudo useradd -m b4iterdev
-sudo adduser b4iterdev sudo
-echo 'b4iterdev:b4iter' | sudo chpasswd
+sudo useradd -m p2pay
+sudo adduser p2pay sudo
+echo 'p2pay:p2pay' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser b4iterdev chrome-remote-desktop
+sudo adduser p2pay chrome-remote-desktop
 } &> /dev/null &&
-printf "\nSetup Complete \nCreated new user b4iterdev with password:b4iter \nYou can change both b4iterdev's password with passwd(with no sudo) \nOr you can change root's password with sudo passwd" >&2 ||
+printf "\nSetup Complete \nCreated new user p2pay with password:p2pay \nYou can change both p2pay's password with passwd(with no sudo) \nOr you can change root's password with sudo passwd" >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless and copy command of Debian Linux \n'
 read -p "Paste Here: " CRP
-su - b4iterdev -c """$CRP"""
+su - p2pay -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
