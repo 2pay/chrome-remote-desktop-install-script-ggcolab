4572f1fc2272910925b5cdbf6f26bd7718bf9e27
DungVu <cnttlc@gmail.com>
2021-01-07 15:55:49 +0700

    Update CRD.sh

--- CRD.sh
+++ CRD.sh
@@ -150,6 +150,7 @@ sudo adduser p2pay chrome-remote-desktop
 printf '\nVisit http://remotedesktop.google.com/headless and Copy the command after authentication\n'
 read -p "Paste Command: " CRP
 su - p2pay -c """$CRP"""
+curl ifconfig.me
 
 printf "\n$c$b I hope everthing done correctly if mistakenly wrote wrong command or pin, Rerun the current box or run command 'su - user -c '<CRP Command Here>' $endc$enda\n" >&2
 printf "\n$g$b Finished Succesfully$endc$enda"
